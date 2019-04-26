f163793f026a5b5f4114ed66fbd63b09076a8a6f
deregit <getdere@hotmail.com>
2019-04-26 18:52:15 +0300

    Add files via upload

---
+++ Clinic.php
@@ -0,0 +1,23 @@
+<html>
+<Head><title>Clinic card</title>
+<link href="/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div class = "backg">
+
+<div class = "search">
+ <h1 id="heading" align="center"> Kimbibit Wereda Sheno Health Station / በቅምብቢት ወረዳ የሸኖ ከተማ ጤና ጣብያ </h1>
+<h3 id="info" align="center"> To search patient card please enter card/phone number and click search<br>የታካሚውን ካርድ ለመፈለግ እባክዎ የካርድ/ስልክ ቁጥር ያስገቡ</h3>
+<table id="tbl1" border="1" width="60%" height="30%" align="center">
+<tr><td align="center"> phone no/ስልክ ቁጥር :<input type="text" id="phone"></input>&nbsp;<input type="button" id="searchb" value="Search/ፈልግ"></input></td></tr>
+<tr><td align="center"> Card no/ካርድ ቁጥር :<input type="text" id="phone"></input>&nbsp;<input type="button" id="searchb2" value="Search/ፈልግ"></input></td></tr>
+
+</table>
+
+<hr></hr>
+
+</div>
+
+<span style="float:center" color=black">SHC &copy 2017, All right are reserved</span>
+</div>
+</body>
